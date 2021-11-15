bc42b22fd42ec88d70e01a0bb5a3890ca53107aa
Igor Ponchielli <igor.ponchielli@gmail.com>
2021-11-15 14:50:47 -0300

    eventos de mouse sendo capturados
    A atividade e inatividade do mouse está sendo monitorada

--- src/appDesktop/App.java
+++ src/appDesktop/App.java
@@ -1,5 +1,9 @@
 package appDesktop;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -11,6 +15,28 @@ public class App {
 
 	public static void main(String[] args) {
 
+		
+		/*TimerTask timerTask = new MyTimerTask();
+        //running timer task as daemon thread
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 10*1000);
+        System.out.println("TimerTask started");
+        //cancel after sometime
+        try {
+            Thread.sleep(120000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        timer.cancel();
+        System.out.println("TimerTask cancelled");
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+		
+		
+		
 		// Clear previous logging configurations.
 		LogManager.getLogManager().reset();
 
@@ -34,6 +60,9 @@ public class App {
 		// Add the appropriate listeners for the example object.
 		GlobalScreen.addNativeMouseListener(captureMouse);
 		GlobalScreen.addNativeMouseMotionListener(captureMouse);
+		GlobalScreen.addNativeMouseWheelListener(captureMouse);
+		
+		captureMouse.verificarAtividadeMouse(3);
 		
 		/*
 		 * String copyright = "\n" +
--- src/appDesktop/CaptureMouse.java
+++ src/appDesktop/CaptureMouse.java
@@ -1,20 +1,151 @@
 package appDesktop;
 
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import org.jnativehook.NativeHookException;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseInputListener;
+import org.jnativehook.mouse.NativeMouseWheelEvent;
+import org.jnativehook.mouse.NativeMouseWheelListener;
 
-public class CaptureMouse implements NativeMouseInputListener {
+public class CaptureMouse implements NativeMouseInputListener, NativeMouseWheelListener {
 
-	private int eventoAtual;
-	private boolean hasStarted = false;
+	Timer timer = new Timer();
+
+	private boolean isActive = false;
+	
+	private int count = 0;
+	private int eventoMouse;
+	private int eventoAtual = 0;
+	private int MouseX, MouseY;
+	private int AtualX = 0, AtualY = 0;
+	
 	TimerTask task;
+
+	public void verificarAtividadeMouse(int tempo) {
+
+		Timer timer = new Timer();
+		
+		
+		task = new TimerTask() {
+			
+
+			public void run() {
+
+					if (isActive == false && (eventoAtual != 9998 && eventoMouse != 9998)) { 
+						
+						System.out.println("--- ATIVO ---");
+						System.out.println("ID:" + eventoAtual + "; Data: " + getDateTimeEvent() + "; Usuario: " + System.getProperty("user.name"));
+						System.out.println("");
+						
+						isActive = true;			
+					}
+					
+					if(eventoAtual != 9999 || (MouseX != AtualX && MouseY != AtualY)) { 
+						count = 0;
+						eventoAtual = eventoMouse;
+						
+						AtualX = MouseX;
+					    AtualY = MouseY;
+					}							
+					
+					if (count <= tempo && eventoAtual != 9998) {						
+						count++;
+						eventoAtual = 9999;							
+					} else {
+						if (eventoAtual != 9998) {
+							
+							System.out.println("--- INATIVO ---");
+							System.out.println("ID:" + eventoAtual + "; Data: " + getDateTimeEvent() + "; Usuario: " + System.getProperty("user.name"));
+							System.out.println("");
+
+							eventoAtual = 9998;
+							eventoMouse = eventoAtual;
+							isActive = false;
+						}
+					}				
+			}
+		};
+		
+		timer.scheduleAtFixedRate(task, 0, 1000); // 1000ms = 1sec
+	}
+	
+	public void nativeMouseClicked(NativeMouseEvent e) {
+//		 System.out.println("Mouse Clicked: " + e.getClickCount());
+//		 System.out.println("MOUSE CLICK: " + e.getID());
+		
+		if (eventoMouse != e.getID()) {
+
+//			System.out.println("--- EVENTO MOUSE CLICAR ---");
+//			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: " + System.getProperty("user.name"));
+//			System.out.println("");
+			
+			eventoMouse = e.getID();
+			eventoAtual = 0;
+		}
+	}
+
+	public void nativeMousePressed(NativeMouseEvent e) {
+
+		if (eventoMouse != e.getID()) {
+			
+//			System.out.println("--- EVENTO MOUSE PRESSIONAR ---");
+//			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: " + System.getProperty("user.name"));
+//			System.out.println("");
+			
+			eventoMouse = e.getID();
+			eventoAtual = 0;
+		}		
+	}
+
+	public void nativeMouseMoved(NativeMouseEvent e) {
+		
+		MouseX = e.getX();
+		MouseY = e.getY();
+//		System.out.println("Mouse Moved: " + e.getX() + ", " + e.getY());
+		
+		if (eventoMouse != e.getID()) {
+				
+//			System.out.println("--- EVENTO MOUSE MOVE ---");
+//			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: " + System.getProperty("user.name"));
+//			System.out.println("");
+			
+			eventoMouse = e.getID();
+			eventoAtual = 0;
+		}
+	}
+
+	public void nativeMouseDragged(NativeMouseEvent e) {
+		
+		if (eventoMouse != e.getID()) {
+			
+//			System.out.println("--- EVENTO MOUSE SOLTAR ---");
+//			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: " + System.getProperty("user.name"));
+//			System.out.println("");
+			
+			eventoMouse = e.getID();
+			eventoAtual = 0;
+		}
+	}
+
+	public void nativeMouseWheelMoved(NativeMouseWheelEvent e) {
+		if (eventoMouse != e.getID()) {
+			
+//			System.out.println("--- EVENTO MOUSE SCROLLAR ---");
+//			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: " + System.getProperty("user.name"));
+//			System.out.println("");
+			
+			eventoMouse = e.getID();
+			eventoAtual = 0;
+		}
+    }
+
+	public void nativeMouseReleased(NativeMouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	private String getDateTimeEvent() {
 
@@ -23,81 +154,5 @@ public class CaptureMouse implements NativeMouseInputListener {
 
 		return dtf.format(now);
 	}
-
-	public void nativeMouseClicked(NativeMouseEvent e) {
-		// System.out.println("Mouse Clicked: " + e.getClickCount());
-		// System.out.println("MOUSE CLICK: " + e.getID());
-
-		if (eventoAtual != e.getID()) {
-			System.out.println("--- EVENTO MOUSE CLICK ---");
-			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: "
-					+ System.getProperty("user.name"));
-			System.out.println("");
-			eventoAtual = e.getID();
-		}
-		if (!hasStarted) {
-			checkInactivity(e.getID(), this.getDateTimeEvent());
-		}
-	}
-
-	public void nativeMousePressed(NativeMouseEvent e) {
-		// System.out.println("Mouse Pressed: " + e.getButton());
-	}
-
-	public void nativeMouseReleased(NativeMouseEvent e) {
-		// System.out.println("Mouse Released: " + e.getButton());
-	}
-
-	public void nativeMouseMoved(NativeMouseEvent e) {
-		// System.out.println("Mouse Moved: " + e.getX() + ", " +
-		// e.getY());
-		if (eventoAtual != e.getID()) {
-			System.out.println("--- EVENTO MOVEU-SE ---");
-			System.out.println("ID: " + e.getID() + " Data: " + this.getDateTimeEvent() + " Usuario: "
-					+ System.getProperty("user.name"));
-			eventoAtual = e.getID();
-		}
-
-		if (!hasStarted) {
-			checkInactivity(e.getID(), this.getDateTimeEvent());
-		}
-	}
-
-	public void nativeMouseDragged(NativeMouseEvent e) {
-		// System.out.println("Mouse Dragged: " + e.getX() + ", " +
-		// e.getY());
-	}
-
-	public void checkInactivity(int eventID, String DateTime) {
-
-		Timer timer = new Timer();
-
-		task = new TimerTask() {
-			private int i = 0;
-
-			public void run() {
-				if (eventID == eventoAtual) {
-					if (i <= 10) {
-						hasStarted = true;
-						System.out.println("" + i++ + " Segundos no mesmo evento");
-					} else {
-						System.out.println("--- INATIVO ---");
-						System.out.println("ID:" + eventoAtual + " Data: " + DateTime + " Usuario: "
-								+ System.getProperty("user.name"));
-						System.out.println("");
-
-						hasStarted = false;
-						timer.cancel();
-					}
-				} else {
-					hasStarted = false;
-					timer.cancel();
-				}
-
-			}
-		};
-		timer.scheduleAtFixedRate(task, 0, 1000); // 1000ms = 1sec
-
-	}
-
+	
 }
